0bf84a97cac84422c9d42e24a4fb77497922a63c
todd_wu <todd404@outlook.com>
2023-10-25 13:58:12 +0800

    🔨解决代码行为不如预期的bug(并发数量超过规定的max_job) 🔧添加服务器监控功能

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 	"sync"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -20,6 +23,10 @@ type Server struct {
 }
 
 type ServerConfig struct {
+	BackendServer struct {
+		Adress string `json:"adress"`
+		Port   int    `json:"port"`
+	} `json:"backend_server"`
 	Servers []Server `json:"servers"`
 }
 
@@ -28,18 +35,23 @@ type Job struct {
 	Body []byte
 }
 
+var config ServerConfig
 var judgeQueue = make(chan Job, 100)
 var jobPool = sync.Pool{
 	New: func() interface{} {
 		return new(Job)
 	},
 }
-var serverReserveMap sync.Map
-var realUuidMap sync.Map
+var serverReserveMap = make(map[Server]int)
+var uuidRealUuidMap sync.Map
+var realUuidUuidMap sync.Map
 var serverMap sync.Map
 
+var serverReserveMapMutex sync.RWMutex
+
 func main() {
-	config, err := loadConfig()
+	var err error
+	config, err = loadConfig()
 	if err != nil {
 		log.Fatalf(fmt.Sprintf("Read config.json fail: %v", err))
 		os.Exit(1)
@@ -47,9 +59,11 @@ func main() {
 
 	initMap(config)
 	go judgeWorker()
+	go monitorServerReserve()
 
 	http.HandleFunc("/judge", handleJudgeRequest)
 	http.HandleFunc("/state", handleStateRequest)
+	http.HandleFunc("/api/set-result", handleSetResult)
 
 	http.ListenAndServe(":7000", nil)
 }
@@ -57,20 +71,20 @@ func main() {
 func initMap(config ServerConfig) {
 	servers := config.Servers
 	for _, s := range servers {
-		serverReserveMap.Store(s, s.MaxJob)
+		serverReserveMap[s] = s.MaxJob
 	}
 }
 
 func loadConfig() (server_config ServerConfig, err error) {
 	_data, _err := os.ReadFile("config.json")
 	if _err != nil {
-		return ServerConfig{nil}, _err
+		return ServerConfig{}, _err
 	}
 
 	var config ServerConfig
 	_err = json.Unmarshal(_data, &config)
 	if _err != nil {
-		return ServerConfig{nil}, _err
+		return ServerConfig{}, _err
 	}
 
 	return config, nil
@@ -81,7 +95,7 @@ func handleJudgeRequest(w http.ResponseWriter, r *http.Request) {
 	defer jobPool.Put(job)
 	job.Uuid = uuid.New().String()
 
-	realUuidMap.Store(job.Uuid, "queuing")
+	uuidRealUuidMap.Store(job.Uuid, "queuing")
 
 	body, err := io.ReadAll(r.Body)
 
@@ -104,7 +118,7 @@ func handleJudgeRequest(w http.ResponseWriter, r *http.Request) {
 
 	res.Uuid = job.Uuid
 
-	w.Header().Set("content-type", "text/json")
+	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(res)
 }
 
@@ -117,8 +131,9 @@ func handleStateRequest(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	uuid := query.Get("uuid")
 
-	realUuid, ok := realUuidMap.Load(uuid)
+	realUuid, ok := uuidRealUuidMap.Load(uuid)
 
+	w.Header().Set("Content-Type", "application/json")
 	if uuid == "" || !ok {
 		res.State = "error uuid"
 		res.Message = "uuid错误..."
@@ -162,33 +177,43 @@ func judgeWorker() {
 		go func(job Job) {
 			var idleServer Server = Server{}
 
-			serverReserveMap.Range(func(key, value any) bool {
-				server := key.(Server)
-				reserve := value.(int)
-
+			serverReserveMapMutex.Lock()
+			for server, reserve := range serverReserveMap {
 				if reserve > 0 {
 					idleServer = server
-					serverReserveMap.Store(server, reserve-1)
-					return false //break
+					serverReserveMap[server]--
+					break
 				}
+			}
+			serverReserveMapMutex.Unlock()
 
-				return true //continue
-			})
-
-			if (idleServer == Server{}) {
+			if idleServer == (Server{}) {
+				//time.Sleep(5 * time.Millisecond)
 				judgeQueue <- job
 				return
 			}
 
-			resp, err := http.Post(fmt.Sprintf("http://%s:%d/judge", idleServer.Adress, idleServer.Port), "application/json", bytes.NewBuffer(job.Body))
+			resp, err := http.Post(
+				fmt.Sprintf("http://%s:%d/judge", idleServer.Adress, idleServer.Port),
+				"application/json",
+				bytes.NewBuffer(job.Body),
+			)
 			if err != nil {
-				realUuidMap.Store(job.Uuid, "error")
+				uuidRealUuidMap.Store(job.Uuid, "error")
+				serverReserveMapMutex.Lock()
+				serverReserveMap[idleServer]++
+				serverReserveMapMutex.Unlock()
+				//TODO: log
 				return
 			}
 
 			defer resp.Body.Close()
 			if resp.StatusCode != http.StatusOK {
-				realUuidMap.Store(job.Uuid, "error")
+				uuidRealUuidMap.Store(job.Uuid, "error")
+				serverReserveMapMutex.Lock()
+				serverReserveMap[idleServer]++
+				serverReserveMapMutex.Unlock()
+				//TODO: log
 				return
 			}
 
@@ -198,8 +223,86 @@ func judgeWorker() {
 
 			json.NewDecoder(resp.Body).Decode(&respData)
 
-			realUuidMap.Store(job.Uuid, respData.Uuid)
+			uuidRealUuidMap.Store(job.Uuid, respData.Uuid)
+			realUuidUuidMap.Store(respData.Uuid, job.Uuid)
 			serverMap.Store(job.Uuid, idleServer)
 		}(job)
+	}
+}
+
+func handleSetResult(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		RealUuid string `json:"uuid"`
+	}
+	json.NewDecoder(r.Body).Decode(&data)
+
+	uuid, ok := realUuidUuidMap.Load(data.RealUuid)
+	if !ok {
+		log.Printf("Foward set result error: wrong uuid")
+		return
+	}
+
+	value, ok := serverMap.Load(uuid)
+	if !ok {
+		log.Printf("Foward set result error: wrong uuid")
+		return
+	}
+	server := value.(Server)
+
+	serverReserveMapMutex.Lock()
+	serverReserveMap[server]++
+	serverReserveMapMutex.Unlock()
+
+	resp, err := http.Post(
+		fmt.Sprintf("http://%s:%d/api/set-result", config.BackendServer.Adress, config.BackendServer.Port),
+		"application/json",
+		r.Body,
+	)
+
+	if err != nil {
+		log.Printf("Report result fail!: %v", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Report result fail with status code: %v", resp.StatusCode)
+	}
+}
+
+func clearTerminal() {
+	optSys := runtime.GOOS
+	if optSys == "linux" {
+		//执行clear指令清除控制台
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("cmd: clear error: %v\n", err.Error())
+		}
+	} else {
+		//执行clear指令清除控制台
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("cmd: cls error: %v\n", err.Error())
+		}
+	}
+}
+
+func monitorServerReserve() {
+	ticker := time.NewTicker(300 * time.Millisecond) // 每 0.3 秒检查一次
+	defer ticker.Stop()
+
+	for range ticker.C {
+		clearTerminal()
+
+		serverReserveMapMutex.RLock()
+		statusLine := ""
+		for server, reserve := range serverReserveMap {
+			// 在这里处理每个服务器的剩余可用量
+			statusLine += fmt.Sprintf("Server: %s:%d, Remaining Jobs: %d\n", server.Adress, server.Port, reserve)
+		}
+		serverReserveMapMutex.RUnlock()
 	}
 }
